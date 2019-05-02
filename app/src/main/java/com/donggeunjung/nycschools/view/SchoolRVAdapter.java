aff76173e46eb5f88e437b44dc85c16bb972901b
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2019-05-02 09:02:03 -0400

    Move binding code from RecyclerViewAdapter into ViewHolder class

--- app/src/main/java/com/donggeunjung/nycschools/view/SchoolRVAdapter.java
+++ app/src/main/java/com/donggeunjung/nycschools/view/SchoolRVAdapter.java
@@ -1,20 +1,16 @@
 package com.donggeunjung.nycschools.view;
 
 import android.arch.lifecycle.Observer;
-import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-
 import com.donggeunjung.nycschools.R;
 import com.donggeunjung.nycschools.databinding.SchoolItemBinding;
 import com.donggeunjung.nycschools.model.SchoolSimple;
 import com.donggeunjung.nycschools.viewmodel.DataViewModel;
-
 import java.util.ArrayList;
 
 /*
@@ -22,10 +18,10 @@ import java.util.ArrayList;
  * Author : DONGGEUN JUNG (Dennis)
  * Date : Apr.16.2019
  */
-public class SchoolRVAdapter extends RecyclerView.Adapter<SchoolRVAdapter.ViewHolder> {
+public class SchoolRVAdapter extends RecyclerView.Adapter<SchoolRVAdapter.SchoolVH> {
     private DataViewModel mViewModel;
     Fragment mParent;
-    RecyclerView.Adapter<SchoolRVAdapter.ViewHolder> mAdapter;
+    RecyclerView.Adapter<SchoolVH> mAdapter;
 
     // Constructor
     public SchoolRVAdapter(DataViewModel viewModel, Fragment parent) {
@@ -45,34 +41,6 @@ public class SchoolRVAdapter extends RecyclerView.Adapter<SchoolRVAdapter.ViewHo
         viewModel.getListSchools().observe(this.mParent, schoolsObserver);
     }
 
-    // Make ViewHolder & View binding object
-    @Override
-    public SchoolRVAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Get the view binding object of custom list item layout
-        LayoutInflater inflater = (LayoutInflater)mParent.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        SchoolItemBinding binding = DataBindingUtil.inflate(inflater,
-                R.layout.school_item, viewGroup, false);
-        // Set the Lifecycle Owner of View binding to fragment
-        binding.setLifecycleOwner(mParent);
-        // Set ViewModel object to binding object as a variable
-        binding.setViewModel(mViewModel);
-        binding.setListFragment((ListFragment)mParent);
-
-        // Make ViewHolder object
-        View view = binding.getRoot();
-        SchoolRVAdapter.ViewHolder vh = new SchoolRVAdapter.ViewHolder(view);
-        // Set binding object to ViewHolder object
-        vh.binding = binding;
-        return vh;
-    }
-
-    // When ViewHolder is binded set data to binding object
-    @Override
-    public void onBindViewHolder(SchoolRVAdapter.ViewHolder viewHolder, int position) {
-        // Set item index number to binding object
-        viewHolder.binding.setIndex(position);
-    }
-
     // Return list items count
     @Override
     public int getItemCount() {
@@ -84,12 +52,43 @@ public class SchoolRVAdapter extends RecyclerView.Adapter<SchoolRVAdapter.ViewHo
         return schools.size();
     }
 
+    // Make ViewHolder & View binding object
+    @Override
+    public SchoolVH onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Get the view binding object of custom list item layout
+        LayoutInflater inflater = LayoutInflater.from(mParent.getContext());
+        SchoolItemBinding binding = DataBindingUtil.inflate(inflater,
+                R.layout.school_item, viewGroup, false);
+        // Set the Lifecycle Owner of View binding to fragment
+        binding.setLifecycleOwner(mParent);
+        // Set ViewModel object to binding object as a variable
+        binding.setViewModel(mViewModel);
+        binding.setListFragment((ListFragment)mParent);
+
+        // Make ViewHolder object
+        return new SchoolVH(binding);
+    }
+
+    // When ViewHolder is binded set data to binding object
+    @Override
+    public void onBindViewHolder(SchoolVH viewHolder, int position) {
+        // Set item index number to binding object
+        viewHolder.bind(position);
+    }
+
     // Reuse views
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class SchoolVH extends RecyclerView.ViewHolder {
         public SchoolItemBinding binding;
 
-        public ViewHolder(View itemView) {
-            super(itemView);
+        //public ViewHolder(View itemView) {
+        public SchoolVH(SchoolItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(int index) {
+            binding.setIndex(index);
+            binding.executePendingBindings();
         }
     }
 }
